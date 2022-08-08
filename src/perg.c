4666f2863327cd03bf69cc689b74d00c3979ea2c
Oliver Calder <oliver@calder.dev>
2022-08-07 20:06:13 -0500

    Switched to using realloc when doubling buffer and carrying over bytes from matches-in-progress, and fixed various bugs.

--- src/perg.c
+++ src/perg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "queue.h"
 #include "nfa.h"
@@ -12,6 +13,29 @@
 
 #define COLOR_RESET ("\e[39;49m")
 
+
+typedef enum {
+    ARG_FLAG_NONE   = 0x00000,
+    ARG_FLAG_I      = 0x00001,  /* case insensitive */
+    ARG_FLAG_V      = 0x00002,  /* invert match */
+    ARG_FLAG_W      = 0x00004,  /* match whole words */
+    ARG_FLAG_X      = 0x00008,  /* match whole lines */
+    ARG_FLAG_O      = 0x00010,  /* only print matching part of lines */
+    ARG_FLAG_HH     = 0x00020,  /* print filename before each match */
+    ARG_FLAG_H      = 0x00040,  /* suppress printing filename before each match */
+    ARG_FLAG_N      = 0x00080,  /* prefix each matching line with line number */
+    ARG_FLAG_A      = 0x00100,  /* treat binary files as text */
+    ARG_FLAG_R      = 0x00200,  /* recursively read all files in each given dir */
+
+    /* Ignored for now */
+    ARG_FLAG_C      = 0x00400,
+    ARG_FLAG_LL     = 0x00800,
+    ARG_FLAG_L      = 0x01000,
+    ARG_FLAG_Q      = 0x02000,
+    ARG_FLAG_AA     = 0x04000,
+    ARG_FLAG_BB     = 0x08000,
+    ARG_FLAG_CC     = 0x10000,
+} arg_flag_t;
 
 typedef enum {
     DEFAULT = 0,
@@ -58,7 +82,7 @@ typedef enum {
 /* Returns number of bytes read, including null terminator.
  * Return value of 0 means EOF, so caller should close file. */
 size_t fill_buffer(FILE *infile, char **buf, size_t *bufsize, int *binary) {
-    char c *tmp_buf;
+    char c;
     size_t bytes_read = 0;
     c = fgetc(infile);
     if (c == EOF || bufsize == 0)
@@ -91,36 +115,29 @@ size_t fill_buffer(FILE *infile, char **buf, size_t *bufsize, int *binary) {
             break;
         }
         /* Buffer filled but not done with line (in text mode), so double buffer size */
-        tmp_buf = malloc(sizeof(char) * (*bufsize) << 1);
-        memcpy(tmp_buf, *buf, *bufsize);
-        free(*buf);
-        *buf = tmp_buf;
         *bufsize <<= 1;
+        *buf = realloc(*buf, sizeof(char) * (*bufsize));
     }
     return bytes_read;
 }
 
 
 match_status_t search_buffer(char *buf, size_t bufsize, nfa_t *nfa, queue_t *match_list){
-    /* Do _not_ overwrite match_list->head or ->tail or assume they are null,
+    /* Do _not_ overwrite match_list->head or ->tail or assume they are NULL,
      * since in text mode, if partial matches exist, they are preserved by
      * maintaining a position in the match list. */
 }
 
 
 size_t preserve_buffer_overlap(char **buf, size_t *bufsize, size_t bytes_read, size_t start) {
-    char *orig_buf = *buf;
-    size_t bytes_remaining, i, bytes_to_preserve = bytes_read - start;
+    size_t i, bytes_to_preserve = bytes_read - start;
     /* If majority of buffer is part of a partial match, double queue size */
     if (bytes_to_preserve > ((*bufsize) >> 1)) {
         (*bufsize) <<= 1;
-        *buf = malloc(sizeof(char) * (*bufsize));
+        *buf = realloc(*buf, sizeof(char) * (*bufsize));
     }
-    bytes_remaining = (*bufsize) - bytes_to_preserve;
     for (i = 0; i < bytes_to_preserve; i++)
-        buf[i] = orig_buf[start + i];
-    if (orig_buf != *buf)
-        free(orig_buf);
+        (*buf)[i] = (*buf)[start + i];
     return bytes_to_preserve;
 }
 
@@ -136,7 +153,7 @@ void print_str_colored(char *str, color_t color, bold_t bold) {
                 break;
         }
     } else {
-        printf(str);
+        printf("%s", str);
     }
 }
 
@@ -147,14 +164,14 @@ void print_from_buffer(char *buf, size_t start, size_t end, color_t color, bold_
     if (color != DEFAULT)
         print_str_colored(buf + start, color, bold);
     else
-        printf(buf + start);
+        printf("%s", buf + start);
     buf[end] = char_to_save;
 }
 
 
-match_status_t search_file(char *filename, FILE *infile, nfa_t *nfa, size_t expr_len) {
-    char *buf;
-    size_t bytes_read, bufsize = 4096, bytes_preserved, i;
+match_status_t search_file(char *filename, FILE *infile, nfa_t *nfa) {
+    char *buf, *fake_buf;
+    size_t bytes_read, bufsize = 4096, bytes_preserved, bytes_remaining, i;
     int binary = 0;
     queue_t match_list;
     match_list_ele_t *cur_match;
@@ -169,7 +186,7 @@ match_status_t search_file(char *filename, FILE *infile, nfa_t *nfa, size_t expr
      */
     buf = malloc(sizeof(char) * bufsize);
     if ((bytes_read = fill_buffer(infile, &buf, &bufsize, &binary)) == ERR_EOF)
-        return;
+        return confirmed_match;
     match_list.head = NULL;
     match_list.tail = NULL;
     while (binary == 0) {
@@ -206,16 +223,19 @@ match_status_t search_file(char *filename, FILE *infile, nfa_t *nfa, size_t expr
                 /* else there were some partial matches, so handle them by
                  * falling through to case MATCH_PROGRESS */
             case MATCH_PROGRESS:    /* only possible if upper limit on bufsize for text files */
+                assert(0);  /* don't bother */
                 /* No full matches, so first match in queue must be in
                  * progress, and must also be the earliest match index */
                 bytes_preserved = preserve_buffer_overlap(&buf, &bufsize, bytes_read, cur_match->start);
                 /* If text, fill_buffer() might changes the bufsize, so the
                  * following parameters might be unchanged by fill_buffer(),
-                 * which is very bad. TODO: fix it
-                 * ... or disallow fixed buffer size for text input */
-                bytes_read = bytes_to_preserve +
-                    fill_buffer(infile, &(buf + bytes_preserved), &bytes_remaining, &binary);
-                /* I don't think this   ^^^^^^^^^^^^^^^^^^^^^^^^  will work... TODO: fix it */
+                 * which is very bad. NOT TRUE, since the whole reason we're
+                 * here is that the buffer size is fixed.
+                 * Nonetheless, disallow fixed buffer size for text input. */
+                fake_buf = buf + bytes_preserved;
+                bytes_remaining = bufsize - bytes_preserved;
+                bytes_read = bytes_preserved +
+                    fill_buffer(infile, &fake_buf, &bytes_remaining, &binary);
                 break;
             case MATCH_NONE:
                 bytes_read = fill_buffer(infile, &buf, &bufsize, &binary);
@@ -231,7 +251,7 @@ match_status_t search_file(char *filename, FILE *infile, nfa_t *nfa, size_t expr
                 confirmed_match = MATCH_FOUND;
                 fprintf(stderr, "Binary file %s matches\n", filename);
                 while (cur_match != NULL) {
-                    (match_list.head) = (void *)(cur_match.next);
+                    (match_list.head) = (void *)(cur_match->next);
                     free(cur_match);
                     cur_match = (match_list_ele_t *)(match_list.head);
                 }
@@ -242,9 +262,10 @@ match_status_t search_file(char *filename, FILE *infile, nfa_t *nfa, size_t expr
                 bytes_preserved = preserve_buffer_overlap(&buf, &bufsize, bytes_read, cur_match->start);
                 /* If binary, fill_buffer() never changes the bufsize, so the
                     * following parameters should be unchanged by fill_buffer() */
-                bytes_read = bytes_to_preserve +
-                    fill_buffer(infile, &(buf + bytes_preserved), &bytes_remaining, &binary);
-                /* I don't think this   ^^^^^^^^^^^^^^^^^^^^^^^^  will work... TODO: fix it */
+                fake_buf = buf + bytes_preserved;
+                bytes_remaining = bufsize - bytes_preserved;
+                bytes_read = bytes_preserved +
+                    fill_buffer(infile, &fake_buf, &bytes_remaining, &binary);
                 break;
             case MATCH_NONE:
                 bytes_read = fill_buffer(infile, &buf, &bufsize, &binary);
@@ -261,8 +282,9 @@ int main(int argc, char *argv[]) {
     struct filepath_node *filepaths_head, *filepath_ptr;
     FILE *infile, *outfile;
     nfa_t *nfa;
+    arg_flag_t flags = ARG_FLAG_NONE;
     /* some code */
-    if (argv < 2) {
+    if (argc < 2) {
         /* ERROR: missing expression */
         exit(1);
     }
@@ -270,12 +292,12 @@ int main(int argc, char *argv[]) {
     /* some code */
     nfa = build_nfa(expression);
     /* some code */
-    if (argc > 2 || R_FLAG) {
+    if (argc > 2 || ARG_FLAG_R) {
         for (filepath_ptr = filepaths_head; filepath_ptr != NULL; filepath_ptr = filepath_ptr->next) {
-            filepath = filepath_ptr->path;
-            FILE *infile = fopen(filepath, "r");
+            FILE *infile = fopen(filepath_ptr->path, "r");
+            search_file(filepath_ptr->path, infile, nfa);
         }
     } else {    /* read from stdin */
-        search_file(stdin, nfa, outfile);
+        search_file("stdin", stdin, nfa);
     }
 }
